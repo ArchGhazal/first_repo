c6a6e76008beec76b679a14545e68083a0ded676
GHAZAL <you@example.com>
2023-01-11 13:45:47 +0300

    exercise_3m_mo

--- exercise_3m.py
+++ exercise_3m.py
@@ -1,7 +1,18 @@
-num1 = 335
-num2 = 66
+num1 = 1122334455666
+num2 = 2.3
+num3 = 1j
+num1_str = str(num1)
+len_str=len(num1_str)
+print(len_str)
+print(num1_str[2])
+print(num1_str[2:6])
+print(str(num2) in num1_str)
+print(str(num3) in num1_str)
+string_with_0 = '0' + num1_str
+print(string_with_0)
+print(string_with_0[:5])
+print(string_with_0[5:])
+print(string_with_0[-4:-1])
 
-if num1 > num2:
-    print(f"{num1} is greater than {num2}")
-else:
-    print(f"{num1} is not greater than {num2}")
+
+
